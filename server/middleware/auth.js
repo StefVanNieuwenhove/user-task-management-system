4e2825491e1c56713778848be97bebe1906014cc
stefvannieuwenhove <stef.vannieuwenhove@telenet.be>
2023-12-05 20:40:03 +0100

    fix: auth middleware chek if token is valid

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,27 +1,32 @@
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const JWT_TOKEN = process.env.JWT_TOKEN;
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const auth = (req, res, next) => {
   try {
-    console.log({ cookies: req.cookies });
     const cookie = req.cookies[JWT_TOKEN];
 
     if (!cookie) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    const token = cookie.split(' ')[1];
-    const decoded = jwt.verify(token, JWT_SECRET);
+    const decoded = jwt.verify(cookie, JWT_SECRET);
+    console.log(decoded);
 
     if (!decoded) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
+    const valid = new Date(decoded.exp * 1000) > new Date();
 
-    req.user = decoded;
+    if (!valid) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
     next();
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
+
+module.exports = auth;
