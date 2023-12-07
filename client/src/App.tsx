458b7eff4575734780794dbf9a7b401179f4caed
stefvannieuwenhove <stef.vannieuwenhove@telenet.be>
2023-12-07 22:22:45 +0100

    fix: now only public routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
-import { Dashboard, Login, Logout } from './pages';
+import { Login, Logout } from './pages';
 import { PrivateRoute, RedirectRoute } from './components';
+import { DashboardProvider } from './context';
 
 function App() {
   return (
@@ -9,15 +10,6 @@ function App() {
         <Route path='/' element={<RedirectRoute path={'login'} />} />
         <Route path='/login' element={<Login />} />
         <Route path='/logout' element={<Logout />} />
-        <Route
-          path='/dashboard'
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-        <Route path='*' element={<RedirectRoute path={'login'} />} />
       </Routes>
     </>
   );
